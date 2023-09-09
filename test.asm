47e7e3577c3d3f9577197fce4ba2bfee2eea77e7
David Benes <dbenes10@gmail.com>
2023-09-09 03:53:57 +0200

    variable and stack oparations in asm

--- test.asm
+++ test.asm
@@ -1,4 +1,27 @@
-global _start
-_start:
-    mov eax, 1 ; move the return value to the eax register
+global _main
+
+segment .text
+_main:
+    ; save stack position
+    mov ebp, esp
+    
+    ; var a = 9;
+    mov eax, 9
+    push eax
+    
+    ; var b = 5;
+    mov eax, 5
+    push eax
+    
+    ; var d;
+    sub esp, 4
+
+    ; var c = 7;
+    mov eax, 7
+    push eax
+
+    ; return d;
+    push DWORD [esp + 1 * 4]
+    pop eax
+    mov esp, ebp ; restore stack position before returning
     ret
