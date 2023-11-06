f5f3c4a24d6dacf40e6282dcd28a71faa76257e8
David Benes <dbenes10@gmail.com>
2023-11-06 23:57:28 +0100

    fixed warnings

--- src/common.h
+++ src/common.h
@@ -5,7 +5,9 @@ typedef int bool;
 #define TRUE 1
 #define FALSE 0
 
-#define UNIMPLEMENTED(x) printf("[NOT IMPLEMENTED] %s:%i in %s() %s\n", __FILE__, __LINE__, __FUNCTION__, (x))
+// NOTE: -pedantic does not support __FUNCTION__
+// #define UNIMPLEMENTED(x) printf("[NOT IMPLEMENTED] %s:%i in %s() %s\n", __FILE__, __LINE__, __FUNCTION__, (x))
+#define UNIMPLEMENTED(x) printf("[NOT IMPLEMENTED] %s:%i: %s\n", __FILE__, __LINE__, (x))
 #define UNUSED(x) (void)(x)
 #define ERROR(loc, msg) printf("[ERROR] %.*s:%i:%i %s\n", (loc).filename.length, (loc).filename.str, (loc).line, (loc).collum, msg)
 
