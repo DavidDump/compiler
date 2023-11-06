2686b1d244ff16dfdb04b116633795a6aabb652b
David Benes <dbenes10@gmail.com>
2023-11-07 00:00:11 +0100

    new Parse() function

--- src/main.c
+++ src/main.c
@@ -119,9 +119,20 @@ typedef struct ASTRoot{
     ASTNode* statements;
     int size;
     int capacity;
-    
-    ASTNode* children; // ???
+
+    Arena mem;
 } ASTRoot;
+
+void ASTRootAddStatement2(ASTRoot* root, ASTNode* node){
+    if(root->size >= root->capacity){
+        size_t newCap = root->capacity * 2;
+        if(newCap == 0) newCap = 1;
+        root->statements = arena_realloc(&root->mem, root->statements, root->capacity * sizeof(root->statements[0]), newCap * sizeof(root->statements[0]));
+        root->capacity = newCap;
+    }
+
+    root->statements[root->size++] = *node;
+}
 
 typedef struct TypeDefinition{
     String symbol;
@@ -178,10 +189,14 @@ ASTNode* NodeInit2(Arena* mem){
 }
 
 void ASTNodePrint2(ASTNode* node, int indent){
+    // TODO: need to fix indentation, everytime a case has a newline it needs to be indented
     for(int i = 0; i < indent; i++){
         printf("  ");
     }
     switch(node->type){
+        case ASTNodeType_COUNT: break;
+        case ASTNodeType_NONE: break;
+        
         case ASTNodeType_INT_LIT: {
             String val = node->node.INT_LIT.value;
             printf("INT LIT: %.*s\n", val.length, val.str);
@@ -192,6 +207,60 @@ void ASTNodePrint2(ASTNode* node, int indent){
             ASTNodePrint2(node->node.EXPRESION.lhs, indent + 1);
             ASTNodePrint2(node->node.EXPRESION.rhs, indent + 1);
         } break;
+        case ASTNodeType_FUNCTION_DEF: {
+            printf("FUNCTION DEF:\n");
+            String id = node->node.FUNCTION_DEF.identifier;
+            String retType = node->node.FUNCTION_DEF.type;
+            ASTNode* stmts = node->node.FUNCTION_DEF.statements;
+            printf("id: %.*s\n", id.length, id.str);
+            printf("ret: %.*s\n", retType.length, retType.str);
+            printf("stmts:");
+            ASTNodePrint2(stmts, indent + 1);
+        } break;
+        case ASTNodeType_VAR_DECL: {
+            printf("VAR DECL:\n");
+            String id = node->node.VAR_DECL.identifier;
+            String type = node->node.VAR_DECL.type;
+            printf("id: %.*s\n", id.length, id.str);
+            printf("type: %.*s\n", type.length, type.str);
+        } break;
+        case ASTNodeType_VAR_DECL_ASSIGN: {
+            printf("VAR DECL ASSIGN:\n");
+            String id = node->node.VAR_DECL_ASSIGN.identifier;
+            String type = node->node.VAR_DECL_ASSIGN.type;
+            ASTNode* expr = node->node.VAR_DECL_ASSIGN.expresion;
+            printf("id: %.*s\n", id.length, id.str);
+            printf("type: %.*s\n", type.length, type.str);
+            printf("expr: \n");
+            ASTNodePrint2(expr, indent + 1);
+        } break;
+        case ASTNodeType_VAR_REASSIGN: {
+            printf("VAR REASSIGN:\n");
+            String id = node->node.VAR_REASSIGN.identifier;
+            ASTNode* expr = node->node.VAR_REASSIGN.expresion;
+            printf("id: %.*s\n", id.length, id.str);
+            printf("expr: \n");
+            ASTNodePrint2(expr, indent + 1);
+        } break;
+        case ASTNodeType_VAR_CONST: {
+            printf("VAR CONST:\n");
+            String id = node->node.VAR_CONST.identifier;
+            String value = node->node.VAR_CONST.value;
+            printf("id: %.*s\n", id.length, id.str);
+            printf("value: %.*s\n", value.length, value.str);
+        } break;
+        case ASTNodeType_RET: {
+            printf("RET:\n");
+            ASTNode* expr = node->node.RET.expresion;
+            ASTNodePrint2(expr, indent + 1);
+        } break;
+    }
+}
+
+void ASTPrint2(ASTRoot root){
+    for(int i = 0; i < root.size; i++){
+        printf("Statement %i\n", i);
+        ASTNodePrint2(&root.statements[i], 0);
     }
 }
 
@@ -255,7 +324,6 @@ ASTNode* parseExpression2_rec(ParseContext* ctx, Arena* mem, ASTNode* lhs, int p
             node->node.EXPRESION.lhs = lhs;
 			node->node.EXPRESION.rhs = rhs;
             lhs = node;
-			// return node;
 		}else{
             break;
         }
@@ -268,9 +336,175 @@ ASTNode* parseExpression2(ParseContext* ctx, Arena* mem){
 	return parseExpression2_rec(ctx, mem, intLit, 5);
 }
 
-// ASTRoot* Parse2(){
+ASTRoot Parse2(ParseContext* ctx, Arena* mem){
+    ASTRoot result = {0};
+    for(Token t = parseConsume2(ctx); t.type != TokenType_NONE; t = parseConsume2(ctx)){
+        switch(t.type){
+            case TokenType_COUNT:
+            case TokenType_NONE: {
+                printf("[ERROR] token type none and count are errors\n");
+                exit(EXIT_FAILURE);
+            } break;
 
-// }
+            case (TokenType_RETURN): {
+                Token next = parsePeek2(ctx, 0);
+                if(next.type == TokenType_INT_LITERAL){
+                    ASTNode* node = NodeInit2(mem);
+                    node->type = ASTNodeType_RET;
+                    node->node.RET.expresion = parseExpression2(ctx, mem);
+                    
+                    // Check for semicolon
+                    // parseConsume2(ctx);
+                    next = parsePeek2(ctx, 0);
+                    if(next.type == TokenType_SEMICOLON){
+                        parseConsume2(ctx);
+                        ASTRootAddStatement2(&result, node);
+                    }else{
+                        ERROR(next.loc, "Statement needs to end with ;");
+                        exit(EXIT_FAILURE);
+                    }
+                }else{
+                    ERROR(next.loc, "Return keyword needs a value to return.");
+                    exit(EXIT_FAILURE);
+                }
+            } break;
+            case (TokenType_IDENTIFIER): {
+                Token next = parsePeek2(ctx, 0);
+                if(next.type == TokenType_INITIALIZER){
+                    // init
+                    parseConsume2(ctx);
+                    next = parsePeek2(ctx, 0);
+                    if(next.type == TokenType_INT_LITERAL){
+                        ASTNode* node = NodeInit2(mem);
+                        node->type = ASTNodeType_VAR_DECL_ASSIGN;
+                        node->node.VAR_DECL_ASSIGN.identifier = t.value;
+                        node->node.VAR_DECL_ASSIGN.expresion = parseExpression2(ctx, mem);
+                        // TODO: add types, figure out type here
+                        // node->node.VAR_DECL_ASSIGN.type = ;
+                        
+                        // TODO: add the variable to the symbol table
+
+                        // Check for semicolon
+                        next = parsePeek2(ctx, 0);
+                        if(next.type == TokenType_SEMICOLON){
+                            parseConsume2(ctx);
+                            ASTRootAddStatement2(&result, node);
+                        }else{
+                            ERROR(next.loc, "Statement needs to end with ;");
+                            exit(EXIT_FAILURE);
+                        }
+                    }else{
+                        ERROR(next.loc, "Variable needs expresion to initialize");
+                        exit(EXIT_FAILURE);
+                    }
+                }else if(next.type == TokenType_COLON){
+                    // decl
+                    parseConsume2(ctx);
+                    next = parsePeek2(ctx, 0);
+                    if(next.type == TokenType_TYPE){
+                        parseConsume2(ctx);
+                        ASTNode* node = NodeInit2(mem);
+                        node->type = ASTNodeType_VAR_DECL;
+                        node->node.VAR_DECL.identifier = t.value;
+                        // TODO: redo types
+                        node->node.VAR_DECL.type = next.value;
+
+                        // TODO: add the variable to the symbol table
+                        
+                        // Check for semicolon
+                        next = parsePeek2(ctx, 0);
+                        if(next.type == TokenType_SEMICOLON){
+                            parseConsume2(ctx);
+                            ASTRootAddStatement2(&result, node);
+                        }else{
+                            ERROR(next.loc, "Statement needs to end with ;");
+                            exit(EXIT_FAILURE);
+                        }
+                    }else{
+                        ERROR(next.loc, "Variable declaration without initializer needs a type");
+                        exit(EXIT_FAILURE);
+                    }
+                }else if(next.type == TokenType_ASSIGNMENT){
+                    // reassign
+                    parseConsume2(ctx);
+                    next = parsePeek2(ctx, 0);
+                    if(next.type == TokenType_INT_LITERAL){
+                        ASTNode* node = NodeInit2(mem);
+                        node->type = ASTNodeType_VAR_REASSIGN;
+                        node->node.VAR_REASSIGN.identifier = t.value;
+                        node->node.VAR_REASSIGN.expresion = parseExpression2(ctx, mem);
+
+                        // TODO: check if the variable is in the symbol table
+
+                        // Check for semicolon
+                        next = parsePeek2(ctx, 0);
+                        if(next.type == TokenType_SEMICOLON){
+                            parseConsume2(ctx);
+                            ASTRootAddStatement2(&result, node);
+                        }else{
+                            ERROR(next.loc, "Statement needs to end with ;");
+                            exit(EXIT_FAILURE);
+                        }
+                    }else{
+                        ERROR(next.loc, "Variable assignment needs an expresion");
+                        exit(EXIT_FAILURE);
+                    }
+                }else if(next.type == TokenType_DOUBLECOLON){
+                    // constant
+                    parseConsume2(ctx); // consume the ::
+                    next = parsePeek2(ctx, 0);
+                    if(next.type == TokenType_INT_LITERAL){
+                        ASTNode* node = NodeInit2(mem);
+                        node->type = ASTNodeType_VAR_CONST;
+                        node->node.VAR_CONST.identifier = t.value;
+                        // TODO: this should be an expresion but only if it is evaluatable during compile time
+                        parseConsume2(ctx);
+                        node->node.VAR_CONST.value = next.value;
+
+                        // TODO: add the variable to the symbol table
+
+                        // Check for semicolon
+                        next = parsePeek2(ctx, 0);
+                        if(next.type == TokenType_SEMICOLON){
+                            parseConsume2(ctx);
+                            ASTRootAddStatement2(&result, node);
+                        }else{
+                            ERROR(next.loc, "Statement needs to end with ;");
+                            exit(EXIT_FAILURE);
+                        }
+                    }else{
+                        ERROR(next.loc, "Variable assignment needs an expresion");
+                        exit(EXIT_FAILURE);
+                    }
+                }else{
+                    // function call
+                }
+            } break;
+
+            case TokenType_TYPE:
+            case TokenType_SEMICOLON:
+            case TokenType_COLON:
+            case TokenType_DOUBLECOLON:
+            case TokenType_INITIALIZER:
+            case TokenType_RARROW:
+            case TokenType_LPAREN:
+            case TokenType_RPAREN:
+            case TokenType_LSCOPE:
+            case TokenType_RSCOPE:
+            case TokenType_LBRACKET:
+            case TokenType_RBRACKET:
+            case TokenType_COMMA:
+            case TokenType_OPERATOR:
+            case TokenType_ASSIGNMENT:
+            case TokenType_COMPARISON:
+            case TokenType_INT_LITERAL:
+                printf("[ERROR] Unhandled token type: %s\n", TokenTypeStr[t.type]);
+            break;
+        }
+    }
+
+    return result;
+}
 
 // 
 // Main entry point
@@ -288,7 +522,7 @@ int main(int argc, char** argv){
     String filename = {.str = argv[1], .length = filenameLen};
     Tokenizer tokenizer = TokenizerInit(sourceRaw, filename);
     TokenArray tokens = Tokenize(&tokenizer);
-    TokensPrint(&tokens);
+    // TokensPrint(&tokens);
 
     char* intType = "int";
     String intSymbol = {.str = intType, .length = 4};
@@ -307,8 +541,8 @@ int main(int argc, char** argv){
     ctx.ops[3] = OperatorDefinitionInit(op4Symbol, 10, type, type, type);
     ctx.opsCount = 4;
 
-    ASTNode* node = parseExpression2(&ctx, &readFileMem);
-    ASTNodePrint2(node, 0);
+    ASTRoot ast = Parse2(&ctx, &readFileMem);
+    ASTPrint2(ast);
 
     #if 0
     Parser parser = {.tokens = tokens}; // uses memory arena
