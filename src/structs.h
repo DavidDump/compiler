fe5496a5cc8597770547b7bd3513b5a9d0127885
DavidLaptop <dbenes10@gmail.com>
2023-11-15 21:53:43 +0100

    prototype code for type flags

--- src/structs.h
+++ src/structs.h
@@ -4,12 +4,28 @@
 #include "string.h"
 #include "arena.h"
 
-typedef struct TypeDefinition{
+// NOTE: this is and idea on how type properties could be solved,
+// maybe this is overkill and its actually enough to just hardcode all the built in types
+// assert that ((unsigned long long) TypeFlags_COUNT) <= ((unsigned long long) (1 << 64))
+typedef enum TypeFlags {
+    TypeFlags_NONE = 0,
+    
+    // meaning the type is an integer, but the byte size may differ
+    TypeFlags_INTEGER = (1 << 0),
+    // meaning the type can be interpreted as a bool
+    TypeFlags_BOOLLIKE = (1 << 1),
+    // the type can be defined as and array
+    TypeFlags_ARRAYABLE = (1 << 2),
+    
+    TypeFlags_COUNT,
+} TypeFlags;
+
+typedef struct TypeDefinition {
     String symbol;
     int byteSize;
 } TypeDefinition;
 
-typedef struct OperatorDefinition{
+typedef struct OperatorDefinition {
     String symbol;
     int precedence;
     TypeDefinition retType;
@@ -17,14 +33,14 @@ typedef struct OperatorDefinition{
     TypeDefinition lhsType;
 } OperatorDefinition;
 
-typedef struct TypeInformation{
+typedef struct TypeInformation {
     TypeDefinition* types;
     int size;
     int capacity;
     Arena mem;
 } TypeInformation;
 
-typedef struct OperatorInformation{
+typedef struct OperatorInformation {
     OperatorDefinition* ops;
     int size;
     int capacity;
