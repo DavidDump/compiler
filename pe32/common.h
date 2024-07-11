4cdf3d302696e3a03e1853cb79cf1dd99e3f330f
David Benes <dbenes10@gmail.com>
2024-07-12 01:02:17 +0200

    common file for types

---
+++ pe32/common.h
@@ -0,0 +1,16 @@
+#ifndef PE_COMMON_H
+#define PE_COMMON_H
+
+#include <stdint.h>
+
+typedef int8_t  s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+#endif // PE_COMMON_H
