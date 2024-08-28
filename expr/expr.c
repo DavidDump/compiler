a25f7126da1907873bb937617974abf81b149fc4
David Benes <dbenes10@gmail.com>
2024-08-28 21:18:54 +0200

    new expression parsing

--- expr/expr.c
+++ expr/expr.c
@@ -14,8 +14,8 @@ typedef struct String {
 typedef enum Operand {
     Operand_NONE,
     Operand_RESERVED,
-    Operand_PLUS,
-    Operand_MINUS,
+    Operand_ADD,
+    Operand_SUB,
     Operand_MUL,
     Operand_DIV,
 } Operand;
@@ -23,11 +23,25 @@ typedef enum Operand {
 typedef enum TokenType {
     TokenType_NONE,
     TokenType_NUMBER,
-    TokenType_PLUS,
-    TokenType_MINUS,
+    TokenType_ADD,
+    TokenType_SUB,
     TokenType_MUL,
     TokenType_DIV,
+    TokenType_OPEN_PAREN,
+    TokenType_CLOSE_PAREN,
+    TokenType_IDENTIFIER,
 } TokenType;
+
+u8* TokenTypeStr[] = {
+    [TokenType_NUMBER] = "NUMBER",
+    [TokenType_ADD] = "ADD",
+    [TokenType_SUB] = "SUB",
+    [TokenType_MUL] = "MUL",
+    [TokenType_DIV] = "DIV",
+    [TokenType_OPEN_PAREN] = "OPEN_PAREN",
+    [TokenType_CLOSE_PAREN] = "CLOSE_PAREN",
+    [TokenType_IDENTIFIER] = "IDENTIFIER",
+};
 
 typedef struct Token {
     TokenType type;
@@ -40,14 +54,16 @@ typedef struct Primary {
 
 typedef enum ExprType {
     ExprType_NONE,
-    ExprType_Primary,
+    ExprType_Number,
     ExprType_Expr,
+    ExprType_Variable,
 } ExprType;
 
 typedef struct Expr {
     ExprType type;
     union {
         Primary primary;
+        Primary variable;
         struct {
             Operand op;
             struct Expr* lhs;
@@ -59,10 +75,10 @@ typedef struct Expr {
 void printOp(Operand op) {
     switch(op) {
         case Operand_NONE: break;
-        case Operand_PLUS: {
+        case Operand_ADD: {
             printf("+");
         } break;
-        case Operand_MINUS: {
+        case Operand_SUB: {
             printf("-");
         } break;
         case Operand_MUL: {
@@ -75,7 +91,7 @@ void printOp(Operand op) {
 }
 
 void printExpr(Expr* value) {
-    if(value->type == ExprType_Primary) {
+    if(value->type == ExprType_Number) {
         printf("%.*s", value->primary.token.value.length, value->primary.token.value.str);
     } else if(value->type == ExprType_Expr) {
         printExpr(value->expr.lhs);
@@ -83,6 +99,19 @@ void printExpr(Expr* value) {
         printExpr(value->expr.rhs);
     } else {
         printf("[ERROR] NONE\n");
+    }
+}
+
+void printTree(Expr* value, u64 indent) {
+    if(value->type == ExprType_Number) {
+        for(int i = 0; i < indent - 1; i++) printf("  ");
+        printf("%.*s\n", value->primary.token.value.length, value->primary.token.value.str);
+    } else if(value->type == ExprType_Expr) {
+        for(int i = 0; i < indent; i++) printf("  ");
+        printOp(value->expr.op);
+        printf("\n");
+        printTree(value->expr.lhs, indent + 1);
+        printTree(value->expr.rhs, indent + 1);
     }
 }
 
@@ -103,28 +132,28 @@ Token parsePeek(ParseContext* ctx) {
     return ctx->tokens[ctx->tokensIndex];
 }
 
-bool isOperator(TokenType token) {
+bool isOperator(Token token) {
     if(
-        token == TokenType_PLUS ||
-        token == TokenType_MINUS ||
-        token == TokenType_MUL ||
-        token == TokenType_DIV
+        token.type == TokenType_ADD ||
+        token.type == TokenType_SUB ||
+        token.type == TokenType_MUL ||
+        token.type == TokenType_DIV
     ) return TRUE;
     return FALSE;
 }
 
-s64 getPresedence(TokenType token) {
-    if(token == TokenType_PLUS || token == TokenType_MINUS) return 5;
-    if(token == TokenType_MUL || token == TokenType_DIV) return 10;
+s64 getPresedence(Token token) {
+    if(token.type == TokenType_ADD || token.type == TokenType_SUB) return 5;
+    if(token.type == TokenType_MUL || token.type == TokenType_DIV) return 10;
 }
 
 Expr* parsePrimary(ParseContext* ctx) {
     Expr* result = arena_alloc(&ctx->mem, sizeof(Expr));
     
-    Token token = parseNext(ctx);
+    Token token = parsePeek(ctx);
     if(token.type == TokenType_NUMBER) {
-        result->type = ExprType_Primary;
-        // NOTE: the value is 0 for now
+        parseNext(ctx);
+        result->type = ExprType_Number;
         result->primary = (Primary){.token = token};
     } else {
         result->type = ExprType_NONE;
@@ -135,17 +164,25 @@ Expr* parsePrimary(ParseContext* ctx) {
 
 Expr* parseExpr_rec(ParseContext* ctx, Expr* lhs, s64 presedence) {
     Token token = parsePeek(ctx);
-    while(isOperator(token.type)) {
-        s64 tokenPresedence = getPresedence(token.type);
+    while(isOperator(token)) {
+        s64 tokenPresedence = getPresedence(token);
         if(tokenPresedence >= presedence) {
             Token op = token;
             parseNext(ctx);
             Expr* rhs = parsePrimary(ctx);
+            if(rhs->type == ExprType_NONE) {
+                printf("[ERROR] expresion error at: %i \"%.*s\"\n", ctx->tokensIndex, ctx->tokens[ctx->tokensIndex].value.length, ctx->tokens[ctx->tokensIndex].value.str);
+                exit(EXIT_FAILURE);
+            }
             token = parsePeek(ctx);
-            while(isOperator(token.type)) {
-                s64 newPresedence = getPresedence(token.type);
+            while(isOperator(token)) {
+                s64 newPresedence = getPresedence(token);
                 if(newPresedence >= tokenPresedence) {
                     rhs = parseExpr_rec(ctx, lhs, tokenPresedence + (newPresedence > tokenPresedence ? 1 : 0));
+                    if(rhs->type == ExprType_NONE) {
+                        printf("[ERROR] expresion error at: %i \"%.*s\"\n", ctx->tokensIndex, ctx->tokens[ctx->tokensIndex].value.length, ctx->tokens[ctx->tokensIndex].value.str);
+                        exit(EXIT_FAILURE);
+                    }
                     token = parsePeek(ctx);
                 } else {
                     break;
@@ -166,6 +203,10 @@ Expr* parseExpr_rec(ParseContext* ctx, Expr* lhs, s64 presedence) {
 
 Expr* parseExpr(ParseContext* ctx) {
     Expr* lhs = parsePrimary(ctx);
+    if(lhs->type == ExprType_NONE) {
+        printf("[ERROR] expresion error at: %i \"%.*s\"\n", ctx->tokensIndex, ctx->tokens[ctx->tokensIndex].value.length, ctx->tokens[ctx->tokensIndex].value.str);
+        exit(EXIT_FAILURE);
+    }
     return parseExpr_rec(ctx, lhs, 0);
 }
 
@@ -173,9 +214,9 @@ bool isNum(u8 c) {
     return ('0' <= c && c <= '9');
 }
 
-// bool isWhitespace(u8 c) {
-//     return (c == ' ' || c == '\n' || c == '\t' || c == '\r' || c == '\v');
-// }
+bool isWhitespace(u8 c) {
+    return (c == ' ' || c == '\n' || c == '\t' || c == '\r' || c == '\v');
+}
 
 typedef struct TokenizeResult {
     Token* tokens;
@@ -189,13 +230,17 @@ TokenizeResult tokenize(Arena* mem, u8* str, u64 strLen) {
     for(u64 i = 0; i < strLen; ++i) {
         u8 c = str[i];
         if(c == '+') {
-            result.tokens[result.count++] = (Token){.type = TokenType_PLUS, .value = (String){.str = &str[i], .length = 1}};
+            result.tokens[result.count++] = (Token){.type = TokenType_ADD, .value = (String){.str = &str[i], .length = 1}};
         } else if(c == '-') {
-            result.tokens[result.count++] = (Token){.type = TokenType_MINUS, .value = (String){.str = &str[i], .length = 1}};
+            result.tokens[result.count++] = (Token){.type = TokenType_SUB, .value = (String){.str = &str[i], .length = 1}};
         } else if(c == '*') {
             result.tokens[result.count++] = (Token){.type = TokenType_MUL, .value = (String){.str = &str[i], .length = 1}};
         } else if(c == '/') {
             result.tokens[result.count++] = (Token){.type = TokenType_DIV, .value = (String){.str = &str[i], .length = 1}};
+        } else if(c == '(') {
+            result.tokens[result.count++] = (Token){.type = TokenType_OPEN_PAREN, .value = (String){.str = &str[i], .length = 1}};
+        } else if(c == ')') {
+            result.tokens[result.count++] = (Token){.type = TokenType_CLOSE_PAREN, .value = (String){.str = &str[i], .length = 1}};
         } else if(isNum(c)) {
             u64 startIndex = i;
             while(isNum(c) && i + 1 < strLen) c = str[++i];
@@ -203,24 +248,106 @@ TokenizeResult tokenize(Arena* mem, u8* str, u64 strLen) {
             u64 endIndex = i;
             u64 len = (endIndex - startIndex) + 1;
             result.tokens[result.count++] = (Token){.type = TokenType_NUMBER, .value = (String){.str = &str[startIndex], .length = len}};
+        } else if(isWhitespace(c)) {
+            continue;
+        } else {
+            printf("[ERROR] unhandled char: %c\n", c);
+            exit(EXIT_FAILURE);
         }
     }
 
     return result;
 }
 
+Expr* parseDecreasingPresedence(ParseContext* ctx, s64 min_prec);
+
+Operand toOperator(Token token) {
+    return (Operand)token.type;
+}
+
+Expr* makeBinary(Arena* mem, Expr* left, Operand op, Expr* right) {
+    Expr* node = arena_alloc(mem, sizeof(Expr));
+    node->type = ExprType_Expr;
+    node->expr.lhs = left;
+    node->expr.rhs = right;
+    node->expr.op = op;
+    return node;
+}
+
+Expr* makeNumber(Arena* mem, Token token) {
+    Expr* node = arena_alloc(mem, sizeof(Expr));
+    node->type = ExprType_Number;
+    node->primary = (Primary){.token = token};
+    return node;
+}
+
+Expr* makeVariable(Arena* mem, Token token) {
+    Expr* node = arena_alloc(mem, sizeof(Expr));
+    node->type = ExprType_Variable;
+    node->primary = (Primary){.token = token};
+    return node;
+}
+
+Expr* parseLeaf(ParseContext* ctx) {
+    Token next = parseNext(ctx);
+    
+    // if is_string_literal(next) return make_string(next)
+    if(next.type == TokenType_NUMBER)          return makeNumber(&ctx->mem, next);
+    if(next.type == TokenType_IDENTIFIER)      return makeVariable(&ctx->mem, next);
+    // if is_open_paren(next)     return parse_expression(0) // NOTE: this is the final parse_expression func
+    // if is_function(next)       return make_function()
+
+    printf("[ERROR] Unhandled input\n");
+}
+
+Expr* parseIncreasingPresedence(ParseContext* ctx, Expr* left, s64 min_prec) {
+    Token next = parsePeek(ctx);
+    if(!isOperator(next)) return left;
+
+    s64 next_prec = getPresedence(next);
+    if(next_prec <= min_prec) {
+        return left;
+    } else {
+        parseNext(ctx);
+        Expr* right = parseDecreasingPresedence(ctx, next_prec);
+        return makeBinary(&ctx->mem, left, toOperator(next), right);
+    }
+}
+
+Expr* parseDecreasingPresedence(ParseContext* ctx, s64 min_prec) {
+    Expr* left = parseLeaf(ctx);
+    
+    while(TRUE) {
+        Expr* node = parseIncreasingPresedence(ctx, left, min_prec);
+        if(node == left) break;
+        left = node;
+    }
+    return left;
+}
+
+Expr* parseExpression(ParseContext* ctx) {
+    return parseDecreasingPresedence(ctx, 0);
+}
+
 int main(void) {
-    u8* input = "1 + 1 / 2";
+    u8* input = "1 - 1 / 2";
+    printf("parsing: %s\n", input);
     u64 inputLen = strlen(input);
     Arena mem = {0};
     TokenizeResult tokens = tokenize(&mem, input, inputLen);
+
+    for(int i = 0; i < tokens.count; i++) {
+        printf("%s\n", TokenTypeStr[tokens.tokens[i].type]);
+    }
 
     ParseContext ctx = {
         .tokens = tokens.tokens,
         .tokensCount = tokens.count,
     };
-    Expr* result = parseExpr(&ctx);
+    // Expr* result = parseExpr(&ctx);
+    Expr* result = parseExpression(&ctx);
 
-    printExpr(result);
+    // printExpr(result);
+    printTree(result, 0);
     return 0;
 }
