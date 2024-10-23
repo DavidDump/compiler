61ab3fabe57a4a9aebe5a63d2dd1b7d65b4ba3a2
DavidLaptop <dbenes10@gmail.com>
2024-10-23 12:02:00 +0200

    started work on integrating the pe32 writer

---
+++ src/peWriter.c
@@ -0,0 +1,341 @@
+#include "peWriter.h"
+#include "bytecode_exe_common.h"
+
+#include <windows.h>
+#include <winnt.h>
+#include <math.h>
+#include <assert.h>
+#include <time.h>
+#include <stdio.h>
+
+#define true TRUE
+#define false FALSE
+
+s32 align(s32 number, s32 alignment) {
+    return (s32)(ceil((double)number / alignment) * alignment);
+}
+
+u64 align_u64(u64 number, u64 alignment) {
+    return (u64)(ceil((double)number / alignment) * alignment);
+}
+
+u32 getFunctionRVA(ImportLibrary* libs, u64 libsCount, String name) {
+    // TODO: instead of iterating all the import use a hashmap
+    for (u64 i = 0; i < libsCount; ++i) {
+        ImportLibrary *lib = &libs[i];
+        for (s32 i = 0; i < lib->functionCount; ++i) {
+            ImportNameToRva *fn = &lib->functions[i];
+            if(StringEquals(fn->name, name)) return fn->iatRva;
+        }
+    }
+    UNREACHABLE("a function was not imported");
+    return 0; // NOTE: just to silence a warning
+}
+
+u32 genDataRVA(HashmapData* userData, String name) {
+    UserDataEntry value;
+    if(hashmapDataGet(userData, name, &value)) {
+        return value.dataRVA;
+    }
+    UNREACHABLE("a data entry could not be found");
+    return 0; // NOTE: just to silence a warning
+}
+
+ParsedDataSection parseDataSection(ImportLibrary* libs, u64 libsCount, HashmapData* userData, IMAGE_SECTION_HEADER *header) {
+    #define get_rva() (s32)(header->VirtualAddress + buffer->size)
+
+    // NOTE: is it better to precalculate this or just use a dynamic array?
+    u64 expected_encoded_size = 0;
+    for (u64 i = 0; i < libsCount; ++i) {
+        ImportLibrary* lib = &libs[i];
+        // Aligned to 2 bytes c string of library name
+        expected_encoded_size += align((s32)lib->dll.name.length + 1, 2);
+        for (s32 h = 0; h < lib->functionCount; ++h) {
+            ImportNameToRva* fn = &lib->functions[h];
+
+            // Ordinal Hint, value not required
+            expected_encoded_size += sizeof(s16);
+            // Aligned to 2 bytes c string of symbol name
+            expected_encoded_size += align((s32)fn->name.length + 1, 2);
+            // IAT placeholder for symbol pointer
+            expected_encoded_size += sizeof(u64);
+            // Image Thunk
+            expected_encoded_size += sizeof(u64);
+        }
+        // NOTE: in the example code this was done once per imported function,
+        // but in the actual emiter we only add one descriptor per imported library, it still works
+        // Import Directory
+        expected_encoded_size += sizeof(IMAGE_IMPORT_DESCRIPTOR);
+
+        // IAT zero-termination
+        expected_encoded_size += sizeof(u64);
+        // Import Directory zero-termination
+        expected_encoded_size += sizeof(u64);
+        // Image Thunk zero-termination
+        expected_encoded_size += sizeof(IMAGE_IMPORT_DESCRIPTOR);
+
+        // Data section
+        for(u64 h = 0; h < userData->size; ++h) {
+            KVPair_SD pair = userData->pair[h];
+            if(pair.key.length != 0) expected_encoded_size += pair.value.dataLen;
+        }
+    }
+
+    ParsedDataSection result = {
+        // FIXME dynamically resize the buffer
+        .buffer = make_buffer(expected_encoded_size, PAGE_READWRITE),
+    };
+
+    Buffer* buffer = &result.buffer;
+    // Function names
+    for (u64 i = 0; i < libsCount; ++i) {
+        ImportLibrary* lib = &libs[i];
+        for (s32 i = 0; i < lib->functionCount; ++i) {
+            ImportNameToRva* function = &lib->functions[i];
+            function->nameRva = get_rva();
+            buffer_append_s16(buffer, 0); // Ordinal Hint, value not required
+            size_t name_size = function->name.length + 1;
+            s32 aligned_name_size = align((s32)name_size, 2);
+            memcpy(
+                buffer_allocate_size(buffer, aligned_name_size),
+                function->name.str,
+                name_size
+            );
+        }
+    }
+
+    // IAT
+    result.iatRva = get_rva();
+    for (u64 i = 0; i < libsCount; ++i) {
+        ImportLibrary* lib = &libs[i];
+        lib->dll.iatRva = get_rva();
+        for (s32 i = 0; i < lib->functionCount; ++i) {
+            ImportNameToRva* fn = &lib->functions[i];
+            fn->iatRva = get_rva();
+            buffer_append_u64(buffer, fn->nameRva);
+        }
+        // End of IAT list
+        buffer_append_u64(buffer, 0);
+    }
+    result.iatSize = (s32)buffer->size;
+
+    // Image Thunks
+    for (u64 i = 0; i < libsCount; ++i) {
+        ImportLibrary* lib = &libs[i];
+        lib->imageThunkRva = get_rva();
+        for (s32 i = 0; i < lib->functionCount; ++i) {
+            ImportNameToRva* fn = &lib->functions[i];
+            buffer_append_u64(buffer, fn->nameRva);
+        }
+        // End of IAT list
+        buffer_append_u64(buffer, 0);
+    }
+
+    // Library Names
+    for (u64 i = 0; i < libsCount; ++i) {
+        ImportLibrary* lib = &libs[i];
+        lib->dll.nameRva= get_rva();
+        size_t name_size = lib->dll.name.length + 1;
+        s32 aligned_name_size = align((s32)name_size, 2);
+        memcpy(
+            buffer_allocate_size(buffer, aligned_name_size),
+            lib->dll.name.str,
+            name_size
+        );
+    }
+
+    // Import Directory
+    result.importDirectoryRva = get_rva();
+    for (u64 i = 0; i < libsCount; ++i) {
+        ImportLibrary* lib = &libs[i];
+
+        IMAGE_IMPORT_DESCRIPTOR *image_import_descriptor = buffer_allocate(buffer, IMAGE_IMPORT_DESCRIPTOR);
+        *image_import_descriptor = (IMAGE_IMPORT_DESCRIPTOR){
+            .OriginalFirstThunk = lib->imageThunkRva,
+            .Name = lib->dll.nameRva,
+            .FirstThunk = lib->dll.iatRva,
+        };
+    }
+    result.importDirectorySize = get_rva() - result.importDirectoryRva;
+    *buffer_allocate(buffer, IMAGE_IMPORT_DESCRIPTOR) = (IMAGE_IMPORT_DESCRIPTOR) {0};
+
+    // Data Section
+    for(u64 i = 0; i < userData->size; ++i) {
+        KVPair_SD pair = userData->pair[i];
+        if(pair.key.length != 0) {
+            UserDataEntry* dataEntry = &userData->pair[i].value;
+            dataEntry->dataRVA = get_rva();
+            
+            buffer_append_u64(buffer, dataEntry->dataLen);
+            u8* bufferMem = buffer_allocate_size(buffer, dataEntry->dataLen);
+            memcpy(bufferMem, dataEntry->data, dataEntry->dataLen);
+        }
+    }
+    
+    assert(buffer->size == expected_encoded_size);
+    assert(buffer->size < UINT32_MAX);
+
+    header->Misc.VirtualSize = (s32)buffer->size;
+    header->SizeOfRawData = (s32)align_u64(buffer->size, PE32_FILE_ALIGNMENT);
+
+    #undef get_rva
+    return result;
+}
+
+// names is the locations where imported functions were used, used for patching
+// dataToPatch is the locations where user defined data was refferenced, used for patching
+Buffer genExecutable(ImportLibrary* libs, u64 libsCount, Buffer bytecode, Buffer names, HashmapData* userData, Buffer dataToPatch) {
+    // Sections
+    IMAGE_SECTION_HEADER sections[] = {
+        {
+            .Name = ".rdata",
+            .Misc = {0},
+            .VirtualAddress = 0,
+            .SizeOfRawData = 0,
+            .PointerToRawData = 0,
+            .Characteristics = IMAGE_SCN_CNT_INITIALIZED_DATA | IMAGE_SCN_MEM_READ,
+        },
+        {
+            .Name = ".text",
+            .Misc = {0},
+            .VirtualAddress = 0,
+            .SizeOfRawData = 0,
+            .PointerToRawData = 0,
+            .Characteristics = IMAGE_SCN_CNT_CODE | IMAGE_SCN_MEM_READ | IMAGE_SCN_MEM_EXECUTE,
+        },
+        {0}
+    };
+
+    IMAGE_SECTION_HEADER *rdata_section_header = &sections[0];
+    IMAGE_SECTION_HEADER *text_section_header  = &sections[1];
+
+    s32 file_size_of_headers =
+        sizeof(IMAGE_DOS_HEADER) +
+        sizeof(s32) + // IMAGE_NT_SIGNATURE
+        sizeof(IMAGE_FILE_HEADER) +
+        sizeof(IMAGE_OPTIONAL_HEADER64) +
+        sizeof(sections);
+    file_size_of_headers = align(file_size_of_headers, PE32_FILE_ALIGNMENT);
+    
+    // prepare .rdata
+    s32 virtual_size_of_headers = align(file_size_of_headers, PE32_SECTION_ALIGNMENT);
+    rdata_section_header->PointerToRawData = file_size_of_headers;
+    rdata_section_header->VirtualAddress = virtual_size_of_headers;
+    ParsedDataSection rdata_section = parseDataSection(libs, libsCount, userData, rdata_section_header);
+
+    // prepare .text
+    u32 codeSizeAligned = align(bytecode.size, PE32_FILE_ALIGNMENT);
+    text_section_header->PointerToRawData = rdata_section_header->PointerToRawData + rdata_section_header->SizeOfRawData;
+    text_section_header->VirtualAddress = rdata_section_header->VirtualAddress + align(rdata_section_header->SizeOfRawData, PE32_SECTION_ALIGNMENT);
+    text_section_header->Misc.VirtualSize = bytecode.size;
+    text_section_header->SizeOfRawData = codeSizeAligned;
+
+    // total size of image
+    s32 virtualSizeOfImage = text_section_header->VirtualAddress + align(text_section_header->SizeOfRawData, PE32_SECTION_ALIGNMENT);
+    u64 max_exe_buffer = file_size_of_headers + rdata_section_header->SizeOfRawData + text_section_header->SizeOfRawData;
+    Buffer exe_buffer = make_buffer(max_exe_buffer, PAGE_READWRITE);
+
+    IMAGE_DOS_HEADER *dos_header = buffer_allocate(&exe_buffer, IMAGE_DOS_HEADER);
+    *dos_header = (IMAGE_DOS_HEADER){
+        .e_magic = IMAGE_DOS_SIGNATURE,
+        .e_lfanew = sizeof(IMAGE_DOS_HEADER),
+    };
+
+    buffer_append_s32(&exe_buffer, IMAGE_NT_SIGNATURE);
+    IMAGE_FILE_HEADER *file_header = buffer_allocate(&exe_buffer, IMAGE_FILE_HEADER);
+    *file_header = (IMAGE_FILE_HEADER){
+        .Machine = IMAGE_FILE_MACHINE_AMD64,
+        .NumberOfSections = ARRAY_SIZE(sections) - 1,
+        .TimeDateStamp = time(NULL),
+        .SizeOfOptionalHeader = sizeof(IMAGE_OPTIONAL_HEADER64),
+        .Characteristics = IMAGE_FILE_EXECUTABLE_IMAGE | IMAGE_FILE_LARGE_ADDRESS_AWARE,
+    };
+
+    IMAGE_OPTIONAL_HEADER64 *optional_header = buffer_allocate(&exe_buffer, IMAGE_OPTIONAL_HEADER64);
+    *optional_header = (IMAGE_OPTIONAL_HEADER64) {
+        .Magic = IMAGE_NT_OPTIONAL_HDR64_MAGIC,
+        .SizeOfCode = text_section_header->SizeOfRawData,
+        .SizeOfInitializedData = rdata_section_header->SizeOfRawData,
+        .AddressOfEntryPoint = 0,
+        .BaseOfCode = text_section_header->VirtualAddress,
+        .ImageBase = 0,                  // NOTE: Does not matter as we are using dynamic base
+        .SectionAlignment = PE32_SECTION_ALIGNMENT,
+        .FileAlignment = PE32_FILE_ALIGNMENT,
+        .MajorOperatingSystemVersion = PE32_MIN_WIN_VERSION,
+        .MinorOperatingSystemVersion = 0,
+        .MajorSubsystemVersion = PE32_MIN_WIN_VERSION,
+        .MinorSubsystemVersion = 0,
+        .SizeOfImage = virtualSizeOfImage,
+        .SizeOfHeaders = file_size_of_headers,
+        .Subsystem = IMAGE_SUBSYSTEM_WINDOWS_CUI, // TODO: allow user to specify this
+        .DllCharacteristics =
+            IMAGE_DLLCHARACTERISTICS_HIGH_ENTROPY_VA |
+            IMAGE_DLLCHARACTERISTICS_NX_COMPAT |
+            IMAGE_DLLCHARACTERISTICS_DYNAMIC_BASE |
+            IMAGE_DLLCHARACTERISTICS_TERMINAL_SERVER_AWARE,
+        .SizeOfStackReserve = 0x100000,
+        .SizeOfStackCommit = 0x1000,
+        .SizeOfHeapReserve = 0x100000,
+        .SizeOfHeapCommit = 0x1000,
+        .NumberOfRvaAndSizes = IMAGE_NUMBEROF_DIRECTORY_ENTRIES,
+        .DataDirectory = {0},
+    };
+    optional_header->DataDirectory[IAT_DIRECTORY_INDEX].VirtualAddress    = rdata_section.iatRva;
+    optional_header->DataDirectory[IAT_DIRECTORY_INDEX].Size              = rdata_section.iatSize;
+    optional_header->DataDirectory[IMPORT_DIRECTORY_INDEX].VirtualAddress = rdata_section.importDirectoryRva;
+    optional_header->DataDirectory[IMPORT_DIRECTORY_INDEX].Size           = rdata_section.importDirectorySize;
+
+#define INVALID_ADDRESS 0xCAFEBABE
+
+    // write IMAGE_SECTION_HEADERs
+    for(u32 i = 0; i < ARRAY_SIZE(sections); ++i) {
+        *buffer_allocate(&exe_buffer, IMAGE_SECTION_HEADER) = sections[i];
+    }
+    
+    // .rdata section
+    exe_buffer.size = rdata_section_header->PointerToRawData;
+    u8* mem = buffer_allocate_size(&exe_buffer, rdata_section.buffer.size);
+    memcpy(mem, rdata_section.buffer.mem, rdata_section.buffer.size);
+    exe_buffer.size = rdata_section_header->PointerToRawData + rdata_section_header->SizeOfRawData;
+
+    // .text segment
+    exe_buffer.size = text_section_header->PointerToRawData;
+
+    u32 begingIndex = exe_buffer.size;
+    u8* beginnigOfCode = buffer_allocate_size(&exe_buffer, codeSizeAligned);
+    memcpy(beginnigOfCode, bytecode.mem, bytecode.size);
+
+    // patch the funcion call locations
+    for(u64 i = 0; i < names.size/sizeof(AddrToPatch); ++i) {
+        AddrToPatch* castNames = (AddrToPatch*)names.mem;
+        u8* addrLoc = &beginnigOfCode[castNames[i].offset];
+        u32 nextInstructonAddr = (begingIndex + castNames[i].offset + 4) - text_section_header->PointerToRawData + text_section_header->VirtualAddress;
+        u32 funcRVA = getFunctionRVA(libs, libsCount, castNames[i].name);
+        
+        addrLoc[0] = ((funcRVA - nextInstructonAddr) >> (8 * 0)) & 0xff;
+        addrLoc[1] = ((funcRVA - nextInstructonAddr) >> (8 * 1)) & 0xff;
+        addrLoc[2] = ((funcRVA - nextInstructonAddr) >> (8 * 2)) & 0xff;
+        addrLoc[3] = ((funcRVA - nextInstructonAddr) >> (8 * 3)) & 0xff;
+    }
+    
+    // patch the data reference locations
+    for(u64 i = 0; i < dataToPatch.size/sizeof(AddrToPatch); ++i) {
+        AddrToPatch* castData = (AddrToPatch*)dataToPatch.mem;
+        u64 offset = castData[i].offset;
+        String name = castData[i].name;
+        
+        u8* addrLoc = &beginnigOfCode[offset];
+        u32 nextInstructonAddr = (begingIndex + offset + 4) - text_section_header->PointerToRawData + text_section_header->VirtualAddress;
+        u32 dataRVA = genDataRVA(userData, name);
+
+        *(u32*)addrLoc += dataRVA - nextInstructonAddr;
+    }
+
+    // patch entry point
+    optional_header->AddressOfEntryPoint = text_section_header->VirtualAddress;
+
+    exe_buffer.size = text_section_header->PointerToRawData + text_section_header->SizeOfRawData;
+
+    return exe_buffer;
+    // TODO: free buffers
+}
---
+++ src/peWriter.h
@@ -0,0 +1,55 @@
+#ifndef COMP_PE_WRITER_H
+#define COMP_PE_WRITER_H
+
+#include "common.h"
+#include "buffer.h"
+#include "hashmap.h"
+
+// NOTE: all the information about the PE32 format is from the MSDN documentation page
+// https://learn.microsoft.com/en-us/windows/win32/debug/pe-format
+
+// NOTE: all parameters of the PE32 file, should be configurable
+#define PE32_FILE_ALIGNMENT 0x200
+#define PE32_SECTION_ALIGNMENT 0x1000
+#define PE32_MIN_WIN_VERSION 6
+
+typedef struct ImportNameToRva {
+    String name;
+    u32 nameRva;
+    u32 iatRva;
+} ImportNameToRva;
+
+typedef struct ImportLibrary {
+    ImportNameToRva dll;
+    ImportNameToRva *functions;
+    s32 functionCount; // TODO: why is this signed?
+    u32 imageThunkRva;
+} ImportLibrary;
+
+enum {
+    EXPORT_DIRECTORY_INDEX,
+    IMPORT_DIRECTORY_INDEX,
+    RESOURCE_DIRECTORY_INDEX,
+    EXCEPTION_DIRECTORY_INDEX,
+    SECURITY_DIRECTORY_INDEX,
+    RELOCATION_DIRECTORY_INDEX,
+    DEBUG_DIRECTORY_INDEX,
+    ARCHITECTURE_DIRECTORY_INDEX,
+    GLOBAL_PTR_DIRECTORY_INDEX,
+    TLS_DIRECTORY_INDEX,
+    LOAD_CONFIG_DIRECTORY_INDEX,
+    BOUND_IMPORT_DIRECTORY_INDEX,
+    IAT_DIRECTORY_INDEX,
+    DELAY_IMPORT_DIRECTORY_INDEX,
+    CLR_DIRECTORY_INDEX,
+};
+
+typedef struct ParsedDataSection {
+    Buffer buffer;
+    s32 iatRva;
+    s32 iatSize;
+    s32 importDirectoryRva;
+    s32 importDirectorySize;
+} ParsedDataSection;
+
+#endif // COMP_PE_WRITER_H
