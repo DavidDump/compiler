afad30940fcdec10b6ddf82838d78b3f7357ad8c
David Benes <dbenes10@gmail.com>
2025-03-08 04:17:18 +0100

    moved common array types to separete file

---
+++ src/commonTypes.h
@@ -0,0 +1,17 @@
+#ifndef COMMON_TYPES_H
+#define COMMON_TYPES_H
+
+#include "common.h"
+#include "dataStructures.h"
+
+defArray(s8);
+defArray(s16);
+defArray(s32);
+defArray(s64);
+
+defArray(u8);
+defArray(u16);
+defArray(u32);
+defArray(u64);
+
+#endif // COMMON_TYPES_H
